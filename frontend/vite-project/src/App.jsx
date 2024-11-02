d4b5198a2f57b50adb6d92dea70704c414e31a06
Aadity Dawkar <dawkarad2002@gmail.com>
2024-11-02 19:49:27 +0530

    added react router dom and axios 2

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OpenHomepage from './components/OpenHomepage';
 import HomePage from './components/HomePage';
-import GoogleAuth from './components/GoogleAuth';
+import GoogleAuth from './components/googleAuth';
 
 function App() {
   return (
